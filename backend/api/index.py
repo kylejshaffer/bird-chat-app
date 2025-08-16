d06a99ce3d866a5561bf948129903005699bc696
Kyle Shaffer <kshaffer@sdl.com>
2025-08-15 17:13:16 -0700

    fixing some flask backend issues

--- backend/api/index.py
+++ backend/api/index.py
@@ -5,7 +5,7 @@ from flask.helpers import send_from_directory
 from flask_cors import CORS
 from rag_system import RAGSystem
 
-app = Flask(__name__, static_folder='bc-app/build', static_url_path='')
+app = Flask(__name__)
 CORS(app)
 
 rag = RAGSystem()
@@ -44,9 +44,5 @@ def reset_vector_store():
     except:
         return status
     
-@app.route("/")
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
-    
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
